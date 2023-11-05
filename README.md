d7414cb9c92f64a4f0d8249eeb1f044ee55284c1
shashi2290 <shashi2290@gmail.com>
2023-11-05 17:59:30 +0530

    shashi-readme

--- README.md
+++ README.md
@@ -1,9 +1,87 @@
 ### Services:
   1. Auth Service
-  2. Product Service
-  3. Order Service
+  2. External API Service - Express Server
+  3. Internal API Service - Express Graphql Server
   
-![ManoSriram - Service jobs](https://ik.imagekit.io/09vbfltqtgx/COM_MICRO-Page-2_qT8AiOXYJ.png)
+![ManoSriram - fury-coder]()
 
 
-![ManoSriram - Flow Chart](https://ik.imagekit.io/09vbfltqtgx/COM_MICRO-Page-1_zQq3E_sKrD.png)
+![Shashi Kant - Flow Chart](https://i.ibb.co/55X2vdL/neblar-backend-drawio.png)
+
+### Steps:
+  1. Clone the repository
+
+  2. Install dependencies with npm install
+    a. in the root folder
+    b. cd auth-service && npm install
+    b. cd external-api-service && npm install
+    c. cd internal-service-graphql && npm install
+
+  3. Update the .env files with correct MONGO_URI to allow server to connect to Mongo db
+
+  4. The API-KEY for internal-service is in the .env file inside the  internal-service-graphql folder
+
+  5. cd auth-service && npm start - to start auth-service
+
+  6. Register a user from POSTMAN 
+      - path /register
+      - port 7070 
+      - payload as  { email, password, name }
+
+  7. Use postman to hit login api to get jwt token
+      - path /login
+      - payload { email, password }
+
+  8. Run Rabbit mq server using docker service
+     docker run -d --hostname my-rabbit --name some-rabbit rabbitmq
+
+  9. Start both Internal(port:5000) and External Service
+     cd into the folder and npm start - in both the services
+
+  10. Using postman hit interval-service-api for crud operations
+    - add Auth Bearer token as headers
+    - http://localhost:5000/graphql - wont work as it will not be able to autheticate the api 
+    - graphql sample query and mutations via graphql variables
+    
+    - ### NOTE**** - if you wish to use http://localhost:5000/graphql then, remove isAuthenticated middleware from /internal-service-graphql/index.js (see - line 47)
+
+    # mutation {
+    #   addPrescription(patient_nhi: "xyz", patient_name: "test4", date: "24 Aug", medication_id: "1234", medication_dosage: "3 times a day") {
+    #     patient {
+    #       nhi
+    #       name
+    #     }
+    #     medication{
+    #       id
+    #       dosage
+    #     }
+    #   }
+    # }
+
+    # {
+    #   prescription(nhi: "123456789") {
+    #     patient {
+    #       name
+    #       nhi
+    #     }
+    #   }
+    # }
+
+    # mutation {
+    #   editPrescription(nhi: "xyz", patient_name: "mounika", medication_id: "abcd", medication_dosage: "6 times a day") {
+    #     patient {
+    #       nhi
+    #       name
+    #     }
+    #     medication {
+    #       id 
+    #       dosage
+    #     }
+    #   }
+    # }
+
+
+
+  11. watch the sync update in the external db
+
+
--- internal-service-graphql/server/schema/schema.test.js
+++ internal-service-graphql/server/schema/schema.test.js
@@ -1,128 +1,128 @@
-const { GraphQLEnumType } = require('graphql');
-const { request, gql } = require('graphql-request');
-const serverUrl = 'http://localhost:5000'; // Replace with your server URL
+// const { GraphQLEnumType } = require('graphql');
+// const { request, gql } = require('graphql-request');
+// const serverUrl = 'http://localhost:5000'; // Replace with your server URL
 
-// Define your GraphQL endpoint
-const endpoint = '/graphql';
+// // Define your GraphQL endpoint
+// const endpoint = '/graphql';
 
-// Sample data for testing
-const sampleClient = {
-  name: 'Sample Client',
-  email: 'sample@example.com',
-  phone: '123-456-7890',
-};
+// // Sample data for testing
+// const sampleClient = {
+//   name: 'Sample Client',
+//   email: 'sample@example.com',
+//   phone: '123-456-7890',
+// };
 
-const sampleProject = {
-  name: 'Sample Project',
-  description: 'Sample Project Description',
-  status: 'new',
-  clientId: 'replace-with-existing-client-id', // Replace with a valid client ID
-};
+// const sampleProject = {
+//   name: 'Sample Project',
+//   description: 'Sample Project Description',
+//   status: 'new',
+//   clientId: 'replace-with-existing-client-id', // Replace with a valid client ID
+// };
 
-// GraphQL request function
-const graphqlRequest = (query, variables = {}) => {
-  return request(serverUrl + endpoint, query, variables);
-};
+// // GraphQL request function
+// const graphqlRequest = (query, variables = {}) => {
+//   return request(serverUrl + endpoint, query, variables);
+// };
 
-describe('GraphQL Mutations', () => {
-  let clientId = null;
-  it('should add a client', async () => {
-    const query = gql`
-      mutation AddClient($name: String!, $email: String!, $phone: String!) {
-        addClient(name: $name, email: $email, phone: $phone) {
-          id
-          name
-          email
-          phone
-        }
-      }
-    `;
+// describe('GraphQL Mutations', () => {
+//   let clientId = null;
+//   it('should add a client', async () => {
+//     const query = gql`
+//       mutation AddClient($name: String!, $email: String!, $phone: String!) {
+//         addClient(name: $name, email: $email, phone: $phone) {
+//           id
+//           name
+//           email
+//           phone
+//         }
+//       }
+//     `;
 
-    const variables = {
-      name: sampleClient.name,
-      email: sampleClient.email,
-      phone: sampleClient.phone,
-    };
+//     const variables = {
+//       name: sampleClient.name,
+//       email: sampleClient.email,
+//       phone: sampleClient.phone,
+//     };
 
-    const {addClient: {id, name}} = await graphqlRequest(query, variables);
-    // Add assertions to check the response
-    expect(id).toBeDefined();
-    expect(name).toBe(sampleClient.name);
-    clientId = id;
-  });
+//     const {addClient: {id, name}} = await graphqlRequest(query, variables);
+//     // Add assertions to check the response
+//     expect(id).toBeDefined();
+//     expect(name).toBe(sampleClient.name);
+//     clientId = id;
+//   });
 
   
 
-  it('should add a project', async () => {
-    const query = gql`
-      mutation AddProject($name: String!, $description: String!, $status: ProjectStatus, $clientId: ID!) {
-        addProject(name: $name, description: $description, status: $status, clientId: $clientId) {
-          id
-          name
-          description
-          status
-          client {
-            id
-          }
-        }
-      }
-    `;
+//   it('should add a project', async () => {
+//     const query = gql`
+//       mutation AddProject($name: String!, $description: String!, $status: ProjectStatus, $clientId: ID!) {
+//         addProject(name: $name, description: $description, status: $status, clientId: $clientId) {
+//           id
+//           name
+//           description
+//           status
+//           client {
+//             id
+//           }
+//         }
+//       }
+//     `;
 
-    const variables = {
-      name: sampleProject.name,
-      description: sampleProject.description,
-      status: sampleProject.status,
-      clientId: clientId,
-    };
+//     const variables = {
+//       name: sampleProject.name,
+//       description: sampleProject.description,
+//       status: sampleProject.status,
+//       clientId: clientId,
+//     };
 
-    const {addProject: {id, name}} = await graphqlRequest(query, variables);
-    // Add assertions to check the response
-    expect(id).toBeDefined();
-    expect(name).toBe(sampleProject.name);
-  });
+//     const {addProject: {id, name}} = await graphqlRequest(query, variables);
+//     // Add assertions to check the response
+//     expect(id).toBeDefined();
+//     expect(name).toBe(sampleProject.name);
+//   });
 
-  it('should delete a client', async () => {
-    const query = gql`
-      mutation DeleteClient($id: ID!) {
-        deleteClient(id: $id) {
-          id
-          name
-          email
-          phone
-        }
-      }
-    `;
+//   it('should delete a client', async () => {
+//     const query = gql`
+//       mutation DeleteClient($id: ID!) {
+//         deleteClient(id: $id) {
+//           id
+//           name
+//           email
+//           phone
+//         }
+//       }
+//     `;
 
-    const variables = {
-      id: clientId, // Replace with a valid client ID
-    };
+//     const variables = {
+//       id: clientId, // Replace with a valid client ID
+//     };
 
-    const {deleteClient: {id}} = await graphqlRequest(query, variables);
-    // Add assertions to check the response
-    expect(id).toBe(clientId);
-  });
+//     const {deleteClient: {id}} = await graphqlRequest(query, variables);
+//     // Add assertions to check the response
+//     expect(id).toBe(clientId);
+//   });
 
-  it('should edit a project', async () => {
-    const query = gql`
-      mutation EditProject($id: ID!, $name: String, $description: String, $status: ProjectStatusUpdate, $clientId: ID) {
-        editProject(id: $id, name: $name, description: $description, status: $status, clientId: $clientId) {
-          id
-          name
-          description
-          status
-        }
-      }
-    `;
+//   it('should edit a project', async () => {
+//     const query = gql`
+//       mutation EditProject($id: ID!, $name: String, $description: String, $status: ProjectStatusUpdate, $clientId: ID) {
+//         editProject(id: $id, name: $name, description: $description, status: $status, clientId: $clientId) {
+//           id
+//           name
+//           description
+//           status
+//         }
+//       }
+//     `;
 
-    const variables = {
-      id: 'replace-with-existing-project-id', // Replace with a valid project ID
-      name: 'Updated Project Name',
-      description: 'Updated Project Description',
-      status: 'In Progress',
-    };
+//     const variables = {
+//       id: 'replace-with-existing-project-id', // Replace with a valid project ID
+//       name: 'Updated Project Name',
+//       description: 'Updated Project Description',
+//       status: 'In Progress',
+//     };
 
-    const response = await graphqlRequest(query, variables);
-    // Add assertions to check the response
-  });
-});
+//     const response = await graphqlRequest(query, variables);
+//     // Add assertions to check the response
+//   });
+// });
 
